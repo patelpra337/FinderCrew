19432e127e348c9941fed75778b5876a7177d3ed
Pravin <patelpra337@gmail.com>
2020-01-16 20:50:21 -0600

    Create new project and files

---
+++ FinderCrew/Model Controller/PersonController.swift
@@ -0,0 +1,9 @@
+//
+//  PersonController.swift
+//  FinderCrew
+//
+//  Created by patelpra on 1/16/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+import Foundation
---
+++ FinderCrew/View Cell/PersonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonTableViewCell.swift
+//  FinderCrew
+//
+//  Created by patelpra on 1/16/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
